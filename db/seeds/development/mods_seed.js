d4cf3d46bb7e10c2a4b8fdc5a32ec5220387a36a
Jeremy Eng <jeng@renoviso.com>
2018-09-12 16:09:36 -0400

    update seeding for mods data

---
+++ db/seeds/development/mods_seed.js
@@ -0,0 +1,19 @@
+const mods = require('../seed_data/Mods.json');
+
+exports.seed = function seedModsDev(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('mods')
+    .del()
+    .then(() => Promise.all(
+      mods.map(mod => knex('mods').insert({
+        name: mod.name,
+        description: mod.description || '',
+        base_drain: mod.baseDrain,
+        fusion_limit: mod.fusionLimit,
+        data: mod.stats || {},
+        type: mod.type,
+        image_name: mod.imageName,
+        polarity: mod.polarity.toLowerCase(),
+      })),
+    ));
+};
