bf5b51a1b47f82ef2542ba5204477575624917d5
Jeremy Eng <jeng@renoviso.com>
2018-09-12 11:54:21 -0400

    refactor knexfile to use template strings

--- knexfile.js
+++ knexfile.js
@@ -3,30 +3,30 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/warframe_arsenal_test',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: `${__dirname}/db/migrations`,
     },
     seeds: {
-      directory: __dirname + '/db/seeds/test'
-    }
+      directory: `${__dirname}/db/seeds/test`,
+    },
   },
   development: {
     client: 'pg',
     connection: 'postgres://localhost/warframe_arsenal',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: `${__dirname}/db/migrations`,
     },
     seeds: {
-      directory: __dirname + '/db/seeds/development'
-    }
+      directory: `${__dirname}/db/seeds/development`,
+    },
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: `${__dirname}/db/migrations`,
     },
     seeds: {
-      directory: __dirname + '/db/seeds/production'
-    }
-  }
+      directory: `${__dirname}/db/seeds/production`,
+    },
+  },
 };
