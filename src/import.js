8022486da94b318d59df699fdd603b582647af68
Jeremy Eng <jeng@renoviso.com>
2018-09-13 16:16:41 -0400

    add warframes to import script

--- src/import.js
+++ src/import.js
@@ -1,5 +1,7 @@
-import knex from '../db/knex';
-import mods from '../lib/warframe-items/data/json/Mods.json';
+import 'app-module-path/cwd';
+import knex from 'db/knex';
+import mods from 'lib/warframe-items/data/json/Mods.json';
+import warframes from 'lib/warframe-items/data/json/Warframes.json';
 
 const modPromises = mods.map(mod => knex.raw(
   `
@@ -21,7 +23,31 @@ const modPromises = mods.map(mod => knex.raw(
   },
 ));
 
-Promise.all(modPromises)
+const warframePromises = warframes.map(warframe => knex.raw(
+  `
+        INSERT INTO warframes (name, description, health, shield, armor, energy, sprint_speed, mastery_requirement, aura_polarity, wikia_url, image_name)
+        VALUES (:name, :description, :health, :shield, :armor, :energy, :sprint_speed, :mastery_requirement, :aura_polarity, :wikia_url, :image_name)
+        ON CONFLICT (name)
+        DO UPDATE SET (description, health, shield, armor, energy, sprint_speed, mastery_requirement, aura_polarity, wikia_url, image_name) 
+        = (:description, :health, :shield, :armor, :energy, :sprint_speed, :mastery_requirement, :aura_polarity, :wikia_url, :image_name)
+        WHERE warframes.name = :name;
+        `,
+  {
+    name: warframe.name,
+    description: warframe.description || '',
+    health: warframe.health,
+    shield: warframe.shield,
+    armor: warframe.armor,
+    energy: warframe.power,
+    sprint_speed: warframe.sprint,
+    mastery_requirement: warframe.masteryReq,
+    aura_polarity: warframe.aura === undefined ? null : warframe.aura,
+    wikia_url: warframe.wikiaUrl,
+    image_name: warframe.imageName,
+  },
+));
+
+Promise.all(modPromises.concat(warframePromises))
   .then(() => {
     process.exit(0);
   })
