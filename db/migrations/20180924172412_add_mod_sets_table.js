05ef8c85cf6ea43f33fe3f0085bbf7b6c06461ac
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-25 11:43:15 -0400

    fix mod_sets table migration

--- db/migrations/20180924172412_add_mod_sets_table.js
+++ db/migrations/20180924172412_add_mod_sets_table.js
@@ -1,14 +1,14 @@
-
 exports.up = function addModSetsTableUp(knex) {
-  return knex.schema.hasTable('warframes').then((exists) => {
+  return knex.schema.hasTable('mod_sets').then((exists) => {
     if (!exists) {
-      return knex.schema
-        .createTable('mod_sets', (t) => {
-          t.increments('mod_set_id').primary();
-          t.string('name').notNullable().unique();
-          t.string('description').notNullable();
-          t.specificType('data', 'jsonb[]').notNullable();
-        });
+      return knex.schema.createTable('mod_sets', (t) => {
+        t.increments('mod_set_id').primary();
+        t.string('name')
+          .notNullable()
+          .unique();
+        t.string('description').notNullable();
+        t.specificType('data', "JSONB DEFAULT '[]'").notNullable();
+      });
     }
   });
 };
