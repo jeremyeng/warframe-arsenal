7405bf16ec35b735a76b117a70fbe24b07161b4c
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-27 17:13:42 -0400

    add noise types seed function

---
+++ db/seeds/development/07_noise_types_seed.js
@@ -0,0 +1,12 @@
+const noiseTypes = require('../seed_data/NoiseTypes.json');
+
+exports.seed = function seedNoiseTypesDev(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('noise_types')
+    .del()
+    .then(() => Promise.all(
+      noiseTypes.map(noiseType => knex('noise_types').insert({
+        noise_type: noiseType,
+      })),
+    ));
+};
