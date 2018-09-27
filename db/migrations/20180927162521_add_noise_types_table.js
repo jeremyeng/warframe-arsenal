d3d26a625df53e20913dbd50e690084b80cb3bf8
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-27 16:47:08 -0400

    add noise types table migration and seed

---
+++ db/migrations/20180927162521_add_noise_types_table.js
@@ -0,0 +1,15 @@
+exports.up = function addNoiseTypesTableUp(knex) {
+  return knex.schema.hasTable('noise_types').then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable('noise_types', (table) => {
+        table
+          .string('noise_type')
+          .primary();
+      });
+    }
+  });
+};
+
+exports.down = function addNoiseTypesTableDown(knex) {
+  return knex.schema.dropTableIfExists('noise_types');
+};
