7016a179c8910b16da456a4f6b29d6b1b5f24341
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-28 14:04:22 -0400

    add trigger types migration and seeds

---
+++ db/migrations/20180927163217_add_trigger_types_table.js
@@ -0,0 +1,15 @@
+exports.up = function addTriggerTypesTableUp(knex) {
+  return knex.schema.hasTable('trigger_types').then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable('trigger_types', (table) => {
+        table
+          .string('trigger_type')
+          .primary();
+      });
+    }
+  });
+};
+
+exports.down = function addTriggerTypesTableDown(knex) {
+  return knex.schema.dropTableIfExists('trigger_types');
+};
---
+++ db/seeds/development/07_trigger_types_seed.js
@@ -0,0 +1,12 @@
+const triggerTypes = require('../seed_data/TriggerTypes.json');
+
+exports.seed = function seedtriggerTypesDev(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('trigger_types')
+    .del()
+    .then(() => Promise.all(
+      triggerTypes.map(triggerType => knex('trigger_types').insert({
+        trigger_type: triggerType,
+      })),
+    ));
+};
