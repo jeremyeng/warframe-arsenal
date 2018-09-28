b856b45b82cf3ee07a81faa7e73ab99bc7310144
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-28 15:24:49 -0400

    add weapon types migration and seed

---
+++ db/migrations/20180927164514_add_weapon_types_table.js
@@ -0,0 +1,15 @@
+exports.up = function addWeaponTypesTableUp(knex) {
+  return knex.schema.hasTable('weapon_types').then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable('weapon_types', (table) => {
+        table
+          .string('weapon_type')
+          .primary();
+      });
+    }
+  });
+};
+
+exports.down = function addWeaponTypesTableDown(knex) {
+  return knex.schema.dropTableIfExists('weapon_types');
+};
---
+++ db/seeds/development/09_weapon_types_seed.js
@@ -0,0 +1,12 @@
+const weaponTypes = require('../seed_data/WeaponTypes.json');
+
+exports.seed = function seedWeaponTypesDev(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('weapon_types')
+    .del()
+    .then(() => Promise.all(
+      weaponTypes.map(weaponType => knex('weapon_types').insert({
+        weapon_type: weaponType,
+      })),
+    ));
+};
