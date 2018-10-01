daed6fe3dc789e25a554a9e7db2b8053e59079b3
Jeremy Eng <jeremy@jeremyeng.com>
2018-09-30 23:18:33 -0400

    add weapons migration and seed files

---
+++ db/migrations/20180928152929_add_weapons_table.js
@@ -0,0 +1,62 @@
+exports.up = function addWeaponsTableUp(knex) {
+  return knex.schema.hasTable('weapons').then((exists) => {
+    if (!exists) {
+      return knex.schema.raw(`
+        CREATE TABLE weapons (
+          buildable_id INTEGER PRIMARY KEY,
+          buildable_type TEXT NOT NULL DEFAULT 'Weapon' CHECK (buildable_type='Weapon'),
+          weapon TEXT,
+          description TEXT,
+          seconds_per_shot DECIMAL,
+          magazine_size INTEGER,
+          ammo INTEGER,
+          reload_time DECIMAL,
+          total_damage DECIMAL,
+          damage_per_second DECIMAL,
+          impact_damage DECIMAL,
+          slash_damage DECIMAL,
+          puncture_damage DECIMAL,
+          toxin_damage DECIMAL,
+          heat_damage DECIMAL,
+          electricity_damage DECIMAL,
+          cold_damage DECIMAL,
+          blast_damage DECIMAL,
+          corrosive_damage DECIMAL,
+          gas_damage DECIMAL,
+          magnetic_damage DECIMAL,
+          radiation_damage DECIMAL,
+          viral_damage DECIMAL,
+          fire_rate DECIMAL,
+          critical_chance DECIMAL,
+          critical_multiplier DECIMAL,
+          trigger TEXT REFERENCES trigger_types (trigger_type),
+          accuracy DECIMAL,
+          status_chance DECIMAL,
+          charge_attack INTEGER,
+          spin_attack INTEGER,
+          leap_attack INTEGER,
+          wall_attack INTEGER,
+          slot INTEGER,
+          noise TEXT REFERENCES noise_types (noise_type),
+          sentinel BOOLEAN NOT NULL,
+          archwing BOOLEAN NOT NULL,
+          vaulted BOOLEAN NOT NULL,
+          mastery_requirement INTEGER,
+          riven_disposition INTEGER,
+          type TEXT REFERENCES weapon_types (weapon_type),
+          category TEXT REFERENCES weapon_categories (weapon_category),
+          channeling DECIMAL,
+          projectile TEXT REFERENCES projectile_types (projectile_type),
+          secondary JSONB,
+          wikia_thumbnail TEXT,
+          wikia_url TEXT,
+          FOREIGN KEY (buildable_id, buildable_type) REFERENCES buildables (buildable_id, buildable_type) 
+        );
+      `);
+    }
+  });
+};
+
+exports.down = function addWeaponsTableDown(knex) {
+  return knex.schema.dropTableIfExists('weapons');
+};
---
+++ db/seeds/development/11_weapons_seed.js
@@ -0,0 +1,67 @@
+const primaries = require('../seed_data/Primary.json');
+const secondaries = require('../seed_data/Secondary.json');
+const melees = require('../seed_data/Melee.json');
+
+const weapons = primaries.concat(secondaries).concat(melees);
+
+exports.seed = function seedWeaponsDev(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('weapons')
+    .del()
+    .then(() => Promise.all(
+      weapons.map(weapon => knex('buildables')
+        .insert({ buildable_type: 'Weapon' })
+        .returning('buildable_id')
+        .then(([buildableId]) => knex('weapons').insert({
+          buildable_id: buildableId,
+          weapon: weapon.name,
+          description: weapon.description,
+          seconds_per_shot: weapon.secondsPerShot,
+          magazine_size: weapon.magazineSize,
+          ammo: weapon.ammo,
+          reload_time: weapon.reloadTime,
+          total_damage: weapon.totalDamage,
+          damage_per_second: weapon.damagePerSecond,
+          impact_damage: weapon.damageTypes.impact,
+          slash_damage: weapon.damageTypes.slash,
+          puncture_damage: weapon.damageTypes.puncture,
+          toxin_damage: weapon.damageTypes.toxin,
+          heat_damage: weapon.damageTypes.heat,
+          electricity_damage: weapon.damageTypes.electricity,
+          cold_damage: weapon.damageTypes.cold,
+          blast_damage: weapon.damageTypes.blast,
+          corrosive_damage: weapon.damageTypes.corrosive,
+          gas_damage: weapon.damageTypes.gas,
+          magnetic_damage: weapon.damageTypes.magnetic,
+          radiation_damage: weapon.damageTypes.radiation,
+          viral_damage: weapon.damageTypes.viral,
+          fire_rate: weapon.fireRate,
+          critical_chance: weapon.criticalChance,
+          critical_multiplier: weapon.criticalMultiplier,
+          trigger: weapon.trigger,
+          accuracy: weapon.accuracy,
+          status_chance: weapon.procChance,
+          charge_attack: weapon.chargeAttack,
+          spin_attack: weapon.spinAttack,
+          leap_attack: weapon.leapAttack,
+          wall_attack: weapon.wallAttack,
+          slot: weapon.slot,
+          noise: weapon.noise,
+          sentinel: weapon.sentinel,
+          archwing: weapon.archwing,
+          mastery_requirement: weapon.masteryReq,
+          riven_disposition: weapon.disposition,
+          type: weapon.type,
+          category: weapon.category,
+          channeling: weapon.channeling,
+          projectile: weapon.projectile,
+          vaulted: weapon.vaulted,
+          secondary: weapon.secondary || weapon.secondaryArea,
+          wikia_thumbnail: weapon.wikiaThumbnail,
+          wikia_url: weapon.wikiaUrl,
+        }))
+        .catch((error) => {
+          Promise.reject(error);
+        })),
+    ));
+};
