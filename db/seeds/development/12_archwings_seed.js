11e729f85c59e6292203a1fb69bd193726e65bd2
Jeremy Eng <jeremy@jeremyeng.com>
2018-10-06 23:20:40 -0400

    naming changes in archwing seed

--- db/seeds/development/12_archwings_seed.js
+++ db/seeds/development/12_archwings_seed.js
@@ -5,19 +5,19 @@ exports.seed = function seedArchwingsDev(knex, Promise) {
   return knex('archwings')
     .del()
     .then(() => Promise.all(
-      archwings.map(warframe => knex('buildables')
+      archwings.map(archwing => knex('buildables')
         .insert({ buildable_type: 'Archwing' })
         .returning('buildable_id')
         .then(([buildableId]) => knex('archwings').insert({
           buildable_id: buildableId,
-          name: warframe.name,
-          description: warframe.description,
-          health: warframe.health,
-          shield: warframe.shield,
-          armor: warframe.armor,
-          energy: warframe.power,
-          wikia_url: warframe.wikiaUrl,
-          image_name: warframe.imageName,
+          name: archwing.name,
+          description: archwing.description,
+          health: archwing.health,
+          shield: archwing.shield,
+          armor: archwing.armor,
+          energy: archwing.power,
+          wikia_url: archwing.wikiaUrl,
+          image_name: archwing.imageName,
         }))),
     ));
 };
