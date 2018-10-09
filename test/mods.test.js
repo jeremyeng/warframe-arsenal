e6abd1c9e499bd345a4654a53c594bca5da0d972
Jeremy Eng <jeremy@jeremyeng.com>
2018-10-08 23:16:22 -0400

    refactor test file

--- test/mods.test.js
+++ test/mods.test.js
@@ -1,75 +1,73 @@
-import axios from 'axios';
+import { graphql } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
 import knex from '../db/knex';
+import schema from '../src/schema';
+import resolvers from '../src/resolvers';
+import loaders from '../src/loaders';
 
-afterAll(() => knex.destroy());
-beforeEach(() => knex.migrate
+beforeAll(() => knex.migrate
   .rollback()
   .then(() => knex.migrate.latest())
   .then(() => knex.seed.run()));
 
-afterEach(() => knex.migrate.rollback());
+afterAll(() => knex.migrate.rollback().then(() => knex.destroy()));
 
+const executableSchema = makeExecutableSchema({ typeDefs: schema, resolvers });
 describe('mods', () => {
   describe('querying mods', () => {
     test('fetch a list of all mods', async () => {
-      const {
-        data: {
-          data: { mods },
-        },
-      } = await axios.post('http://localhost:8000/graphql', {
-        query: `
-            {
-              mods {
-              id
-              name
-              description
-              baseDrain
-              fusionLimit
-              type
-              polarity
-              data
-            }
+      const query = `
+        query {
+          mods {
+            id
+            name
+            description
+            baseDrain
+            fusionLimit
+            type
+            polarity
+            data
           }
-          `,
-      });
+        }
+      `;
+      const {
+        data: { mods },
+      } = await graphql(executableSchema, query, {}, { knex, loaders });
 
-      expect(mods.length).toBe(830);
+      expect(mods.length).toBe(828);
     });
 
-    test('fetch a mod by its id', () => axios
-      .post('http://localhost:8000/graphql', {
-        query: `
-            {
-              mod(id:203) {
-              id
-              name
-              description
-              baseDrain
-              fusionLimit
-              type
-              polarity
-              data
-            }
+    test('fetch a mod by its id', async () => {
+      const query = `
+        query {
+          mod(id:203) {
+            id
+            name
+            description
+            baseDrain
+            fusionLimit
+            type
+            polarity
+            data
           }
-          `,
-      })
-      .then((result) => {
-        expect(result.data).toMatchObject({
-          data: {
-            mod: {
-              id: '203',
-              name: 'Energy Inversion',
-              description: '+30% Shield Capacity',
-              baseDrain: 2,
-              fusionLimit: 3,
-              type: 'Archwing',
-              polarity: 'vazarin',
-              data: {
-                effect: ['+30%', '+60%', '+90%', '+120%'],
-              },
-            },
-          },
-        });
-      }));
+        }
+      `;
+      const {
+        data: { mod },
+      } = await graphql(executableSchema, query, {}, { knex, loaders });
+
+      expect(mod).toMatchObject({
+        id: '203',
+        name: 'Energy Inversion',
+        description: '+30% Shield Capacity',
+        baseDrain: 2,
+        fusionLimit: 3,
+        type: 'Archwing',
+        polarity: 'vazarin',
+        data: {
+          effect: ['+30%', '+60%', '+90%', '+120%'],
+        },
+      });
+    });
   });
 });
