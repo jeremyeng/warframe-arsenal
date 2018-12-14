991bfe64ab89416e5ae2ca7ced585bdb2983fcd4
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-13 23:50:38 -0500

    change database search path settings

--- db/migrations/0_create_schemas.js
+++ db/migrations/0_create_schemas.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.raw(`
     CREATE SCHEMA warframe_arsenal_public;
     CREATE SCHEMA warframe_arsenal_private;
-    SET search_path TO warframe_arsenal_public, public;
+    ALTER DATABASE warframe_arsenal SET search_path = warframe_arsenal_public, public;
     ALTER DEFAULT privileges REVOKE EXECUTE ON functions FROM public;
   `);
 };
@@ -11,5 +11,6 @@ exports.down = function(knex) {
   return knex.raw(`
     DROP SCHEMA warframe_arsenal_public CASCADE;
     DROP SCHEMA warframe_arsenal_private CASCADE;
+    ALTER DATABASE warframe_arsenal RESET search_path;
   `);
 };
