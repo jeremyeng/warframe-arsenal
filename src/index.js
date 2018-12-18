d5c124e0bf7360a4d4fde89e777e14c79af1739a
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-17 23:02:44 -0500

    add user env variable for db connection

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.use(
     {
       host: process.env.DATABASE_URL,
       database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
     },
     'warframe_arsenal_public',
