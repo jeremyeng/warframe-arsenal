b746c8c91f02f24f0052dd06e1d53f15835de607
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-19 09:31:28 -0500

    make postgraphile role able to login

--- db/migrations/20180924222552_create_postgraphile_role.js
+++ db/migrations/20180924222552_create_postgraphile_role.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-  return knex.schema.raw(`CREATE ROLE postgraphile`);
+  return knex.schema.raw(`CREATE ROLE postgraphile LOGIN`);
 };
 
 exports.down = function(knex) {
