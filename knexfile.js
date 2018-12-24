470eecf871192e81da78d617caa15e8458d6aff2
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-24 13:49:49 -0500

    change seeds directory for production knexfile

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
       directory: `${__dirname}/db/migrations`,
     },
     seeds: {
-      directory: `${__dirname}/db/seeds/production`,
+      directory: `${__dirname}/db/seeds`,
     },
   },
 };
