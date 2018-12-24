c76588e660f7389acbb349d58be50532748a9669
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-24 14:17:56 -0500

    intermediate commit

--- src/index.js
+++ src/index.js
@@ -16,33 +16,33 @@ async function migrateUpToLatest() {
 async function startServer() {
   migrateUpToLatest();
 
-  const app = express();
-  app.use(
-    postgraphile(
-      {
-        host: process.env.DATABASE_URL,
-        database: process.env.DATABASE_NAME,
-        user: process.env.POSTGRAPHILE_DATABASE_USER,
-        password: process.env.POSTGRAPHILE_DATABASE_PASSWORD,
-      },
-      'warframe_arsenal_public',
-      {
-        graphiql: true,
-        enhanceGraphiql: true,
-        pgDefaultRole: 'guest',
-        jwtPgTypeIdentifier: 'warframe_arsenal_public.jwt',
-        jwtSecret:
-          '29E357812E4D2664C6FD9764D28B9BA958CC995D216C0C8A837039D7C6B43FD0',
-      },
-    ),
-  );
+  // const app = express();
+  // app.use(
+  //   postgraphile(
+  //     {
+  //       host: process.env.DATABASE_URL,
+  //       database: process.env.DATABASE_NAME,
+  //       user: process.env.POSTGRAPHILE_DATABASE_USER,
+  //       password: process.env.POSTGRAPHILE_DATABASE_PASSWORD,
+  //     },
+  //     'warframe_arsenal_public',
+  //     {
+  //       graphiql: true,
+  //       enhanceGraphiql: true,
+  //       pgDefaultRole: 'guest',
+  //       jwtPgTypeIdentifier: 'warframe_arsenal_public.jwt',
+  //       jwtSecret:
+  //         '29E357812E4D2664C6FD9764D28B9BA958CC995D216C0C8A837039D7C6B43FD0',
+  //     },
+  //   ),
+  // );
 
-  app.listen({ port: process.env.PORT || 8081 }, () => {
-    // eslint-disable-next-line no-console
-    console.log(
-      `Server running on http://localhost:${process.env.PORT || 8081}/graphiql`,
-    );
-  });
+  // app.listen({ port: process.env.PORT || 8081 }, () => {
+  //   // eslint-disable-next-line no-console
+  //   console.log(
+  //     `Server running on http://localhost:${process.env.PORT || 8081}/graphiql`,
+  //   );
+  // });
 }
 
 startServer();
