6d5ce461722097b30cae094a16166b6ff7d60ee1
Jeremy Eng <jeremy@jeremyeng.com>
2018-12-30 20:17:55 -0500

    make postgraphile database password an environment variable

--- db/migrations/20180924222552_create_postgraphile_role.js
+++ db/migrations/20180924222552_create_postgraphile_role.js
@@ -1,5 +1,9 @@
 exports.up = function(knex) {
-  return knex.schema.raw(`CREATE ROLE postgraphile LOGIN PASSWORD 'tenno'`);
+  return knex.schema.raw(
+    `CREATE ROLE postgraphile LOGIN PASSWORD '${
+      process.env.POSTGRAPHILE_DATBASE_PASSWORD
+    }'`,
+  );
 };
 
 exports.down = function(knex) {
